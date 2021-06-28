897ac639c34a80e5f0b6889fe58edb63f9c63f06
黒田明 <kurodaakira.jp@gmail.com>
2021-06-28 16:46:57 +0800

    润色shell交互

--- u2-bot.sh
+++ u2-bot.sh
@@ -232,7 +232,7 @@ cloudflare_st() {
 	echo ${BESTIP} > nowip.txt
 	echo -e "\n旧 IP 为 ${NOWIP}\n${Yellow_font_prefix}新 IP 为 ${BESTIP}${Font_color_suffix}\n"
 
-	echo -e "${Green_font_prefix}> 开始备份 Hosts 文件 (hosts_backup)...${Font_color_suffix}";
+	echo -e "${Green_font_prefix}> 开始备份 Hosts 文件 (/etc/hosts.bk)...${Font_color_suffix}";
 	\cp -f /etc/hosts /etc/hosts.bk
 
 	echo -e "${Green_font_prefix}> 开始替换...${Font_color_suffix}";
